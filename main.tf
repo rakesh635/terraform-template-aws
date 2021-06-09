dd508c94e06ea0e8c648eedbe7eb00dc4c058861
rakesh635 <rakesh635@gmail.com>
2021-06-09 15:52:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,56 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = "~> 0.14"
+
+  backend "remote" {
+    organization = "REPLACE_ME"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
 }
- 
-resource "aws_instance" "kaeptn-eichhorn" {
-  ami = "ami-13b8337c"
-  instance_type = "t2.micro"
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+
+resource "random_pet" "sg" {}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
