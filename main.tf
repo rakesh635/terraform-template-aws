414f7f8372165bf3389af4a58522a0db9d5702b0
rakesh635 <rakesh635@gmail.com>
2022-08-29 13:56:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0729e439b6769d6ab"
+  ami                    = "ami-0acaeae800b342c66"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
